8568b6834281c67cd98f4031287c20440ed97a6e
vagrant <carol15022@hotmail.com>
2014-12-06 03:05:24 +0000

    Cookbook Job Chef

--- cookbooks/radar/recipes/jenkins.rb
+++ cookbooks/radar/recipes/jenkins.rb
@@ -1,31 +1,26 @@
+jenkins_plugin 'git'
 
+xml_radar = File.join(Chef::Config[:file_cache_path], 'jobRadar_config.xml')
 
-xml = File.join(Chef::Config[:file_cache_path], 'jenkins_config.xml')
-
-template xml do
-	source 'jenkins_config.xml.erb'
+template xml_radar do
+	source 'jobRadar_config.xml.erb'
 end
 
-jenkins_job 'radar' do
-	config xml
+jenkins_job 'teste_radar' do
+	config xml_radar
 	action :create
 end	
 
+xml_chef = File.join(Chef::Config[:file_cache_path], 'jobChef_config.xml')
 
+template xml_chef do
+	source 'jobChef_config.xml.erb'
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+jenkins_job 'teste_chef' do 
+	config xml_chef
+	action :create
+end
 
 
 jenkins_command "safe-restart" 
