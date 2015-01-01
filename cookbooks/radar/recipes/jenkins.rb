8eb94dbd3a80d33105206b1c521b3f0d5af26758
Leonardo Leite <leonardofl87@gmail.com>
2015-01-01 17:54:18 -0200

    ajustes na bootstrap do nó

--- cookbooks/radar/recipes/jenkins.rb
+++ cookbooks/radar/recipes/jenkins.rb
@@ -5,28 +5,28 @@
 user = node['radar']['user']
 password = node['radar']['server_password']
 home = "/home/#{user}"
-venv_folder = "#{home}/radar/venv_radar"
+venv_folder = "#{home}/venv_jenkins"
 
 jenkins_plugin 'git'
 
 xml_radar = File.join(Chef::Config[:file_cache_path], 'jobRadar_config.xml')
 
-template xml_radar do
-  source 'jobRadar_config.xml.erb'
+template xml_build do
+  source 'jobBuildRadar_config.xml.erb'
   variables ({
     :venv_folder => venv_folder
   })
 end
 
 jenkins_job 'build_radar' do
-  config xml_radar
+  config xml_build
   action :create
 end	
 
-xml_chef = File.join(Chef::Config[:file_cache_path], 'jobChef_config.xml')
+xml_deploy = File.join(Chef::Config[:file_cache_path], 'jobDeployRadar_config.xml')
 
-template xml_chef do
-  source 'jobChef_config.xml.erb'
+template xml_deploy do
+  source 'jobDeployRadar_config.xml.erb'
   variables ({
     :home => home,
     :venv_folder => venv_folder
@@ -48,7 +48,7 @@ end
 
 
 jenkins_job 'deploy_radar' do 
-  config xml_chef
+  config xml_deploy
   action :create
 end
 
