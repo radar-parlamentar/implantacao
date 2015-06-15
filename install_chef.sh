72c6fdc4ed61ef62bf7a66a2a53816e6ad21af81
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2015-06-15 17:42:54 -0300

    Ajustando o install_chef para contemplar sistemas que não possuem o executável sudo

--- install_chef.sh
+++ install_chef.sh
@@ -1,7 +1,15 @@
 #! /bin/bash
 #License: GPLv3
 
-sudo apt-get update
-sudo apt-get install -y ruby1.9.1 ruby1.9.1-dev build-essential
-sudo gem install chef -v 11.16
+#Check if sudo command exists.
+# If it doesn't, use the su command
 
+if which sudo &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y ruby1.9.1 ruby1.9.1-dev build-essential
+    sudo gem install chef -v 11.16
+else
+    su -c 'apt-get update &&
+            apt-get install -y ruby1.9.1 ruby1.9.1-dev build-essential &&
+            gem install chef -v 11.16'
+fi
